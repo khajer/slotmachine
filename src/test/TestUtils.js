1f528fb119329a4d9f5d17bac26231bc942c9d98
kha <itsaraK>
2022-05-23 16:47:04 +0700

    add check array

--- src/test/TestUtils.js
+++ src/test/TestUtils.js
@@ -1,3 +1,31 @@
+// ----------------------------------------------
+// https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+// ----------------------------------------------
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+// --------------
+
 var txtLabel = "";
 var describe = function(str, fn){
     txtLabel = str;
@@ -5,22 +33,43 @@ var describe = function(str, fn){
 }
 var assert = {
     equal(val1, val2){
-        var txt = txtLabel + ": [";
-        if(val1 === val2) {
-            txt += "✓] success"
-        }else{
-            txt += "x] fails"
+        var txt = txtLabel + "\n [";
+        if(typeof (val1) == "object" 
+            && Array.isArray(val1)){
+            if(val1.equals(val2)){
+                txt += "✓] success"
+            }else{
+                txt += "x] fails"
+            }
+        }else{            
+            if(val1 === val2) {
+                txt += "✓] success"
+            }else{
+                txt += "x] fails"
+            }           
         }
         console.log(txt);
     },
     notEqual(val1, val2){
-        var txt = txtLabel + ": [";
-        if(val1 !== val2) {
-            txt += "✓] success"
-        }else{
-            txt += "x] fails"
-        }
-        console.log(txt);
+        var txt = txtLabel + "\n [";
+        if(typeof (val1) == "object" 
+            && Array.isArray(val1)){
+            if(val1.equals(val2)){
+                txt += "x] fails"
+            }else{                
+                txt += "✓] success"
+            }
+        }else{            
+            if(val1===val2){
+                txt += "✓] success"
+            }else{
+                txt += "x] fails"
+            }
+            
+        } 
+        console.log(txt);       
     }
 }
+
+
 export{describe, assert}
--- src/test/test.js
+++ src/test/test.js
@@ -2,7 +2,6 @@ import {Logic} from '../Logic.js';
 
 import {describe, assert} from './TestUtils.js';
 
-
 describe('Logic.genData(): test length = 15', ()=>{    
     assert.equal(15, Logic.genData().length);
 });
@@ -11,3 +10,20 @@ describe('Logic.genData(): test length != 0', ()=>{
     assert.notEqual(0, Logic.genData().length);
 });
 
+describe('Logic.splitDataToSlot(): check first array == 5 column', ()=>{    
+    var data = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+    var sp = Logic.splitDataToSlot(data);
+    assert.equal(5, sp.length);
+});
+
+describe('Logic.checkDataRule()', ()=>{
+    var data = [1, 1, 1, 1, 1, 
+                0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0]
+    
+    var dataExpectSlot = [];
+    dataExpectSlot[0] = [0];
+    dataExpectSlot[1] = [0];
+    dataExpectSlot[2] = [0];
+    assert.equal(Logic.checkDataRule(data)[0], dataExpectSlot[0]);
+});
